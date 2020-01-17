eb61ecf6f834f86c2f90beedf4c4a0acc7bf4e58
Luca Gallois <luca.gallois@synolia.com>
2020-01-17 14:49:37 +0100

    project init

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Synolia\SyliusMailTesterPlugin\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('synolia_sylius_mail_tester_plugin');
+        if (\method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('synolia_sylius_mail_tester_plugin');
+        }
+
+        return $treeBuilder;
+    }
+}
