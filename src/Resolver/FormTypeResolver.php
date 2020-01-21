e69bb933b2ecd2a6894878408e7da194393b03ee
Nicolas MELONI <nicolas.meloni@synolia.com>
2020-01-21 10:07:10 +0100

    added form type resolver

---
+++ src/Resolver/FormTypeResolver.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Synolia\SyliusMailTesterPlugin\Resolver;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Form\FormTypeInterface;
+
+final class FormTypeResolver
+{
+    /** @var \Doctrine\Common\Collections\ArrayCollection<int, ResolvableFormTypeInterface> */
+    private $formTypes;
+
+    public function getFormType(string $emailKey): FormTypeInterface
+    {
+        /** @var ResolvableFormTypeInterface $formType */
+        foreach ($this->formTypes as $formType) {
+            if ($formType->support($emailKey)) {
+                return $formType->getFormType($emailKey);
+            }
+        }
+
+        throw new NoResolvableFormTypeFoundException(sprintf('No resolvable form found for email %s.', $emailKey));
+    }
+
+    public function getAllFormTypes(): iterable
+    {
+        return $this->formTypes;
+    }
+
+    public function addFormType(ResolvableFormTypeInterface $resolvableForm): void
+    {
+        if (null === $this->formTypes) {
+            $this->formTypes = new ArrayCollection();
+        }
+        if (false === $this->formTypes->contains($resolvableForm)) {
+            $this->formTypes->add($resolvableForm);
+        }
+    }
+}
